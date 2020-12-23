ea1a79e19da4a50c8afec3be4be80026e752fd8d
Victor COMBAT <victor.cmbt@gmail.com>
2020-12-23 15:05:05 +0100

    :sparkles: Chapter 1 - Firsts | First Import of a Model

--- Chapter 1 - Firsts/script.js
+++ Chapter 1 - Firsts/script.js
@@ -1,13 +1,24 @@
-const createScene = () => {
-    const scene = new BABYLON.Scene(engine);
+var createScene = function () {
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
 
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
 
+    // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
 
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
-    const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    BABYLON.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "both_houses_scene.babylon").then((result) => {
+        const house1 = scene.getMeshByName("detached_house");
+        house1.position.y = 2;
+        const house2 = result.meshes[2];
+        house2.position.y = 1;
+    });
 
     return scene;
-}
+};
