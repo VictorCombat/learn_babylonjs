4cf97da96a7d8a9f1ed89c74e2e40cd8a2340fcb
Victor COMBAT <victor.cmbt@gmail.com>
2020-12-23 16:21:47 +0100

    :sparkles: Chapter 2 - Build A Village | Copying Meshes

---
+++ Chapter 2 - Build A Village/import_village.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+const createScene = function () {
+
+    const scene = new BABYLON.Scene(engine);
+
+    /* SET CAMERA & LIGHT */
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+    camera.attachControl(canvas, true);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+    BABYLON.SceneLoader.ImportMeshAsync("", "./", "village.glb");
+
+    return scene;
+};
+
+const scene = createScene(); //Call the createScene function
+
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+        scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+        engine.resize();
+});
+
