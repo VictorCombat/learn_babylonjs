77980560d5571b04792fb07d46f658ac34131fa3
Victor COMBAT <victor.cmbt@gmail.com>
2020-12-24 14:16:27 +0100

    :sparkles: Chapter 3 - Village Animation | Car Animation

--- Chapter 3 - Village Animation/script.js
+++ Chapter 3 - Village Animation/script.js
@@ -10,7 +10,38 @@ const createScene = function () {
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
 
+    BABYLON.SceneLoader.ImportMeshAsync("", "./", "village.glb");
     BABYLON.SceneLoader.ImportMeshAsync("", "./", "car.babylon").then(() => {
+        const car = scene.getMeshByName("car");
+        car.rotation = new BABYLON.Vector3(-Math.PI / 2, 0, Math.PI / 2);
+        car.position.y = 0.16;
+        car.position.x = 3;
+        car.position.z = -8;
+
+        const animCar = new BABYLON.Animation("carAnimation", "position.z", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+        const carKeys = [];
+        carKeys.push({
+            frame: 0,
+            value: 8
+        });
+        carKeys.push({
+            frame: 150,
+            value: -7
+        });
+        carKeys.push({
+            frame: 200,
+            value: -7
+        });
+        animCar.setKeys(carKeys);
+
+        car.animations = [];
+        car.animations.push(animCar);
+
+        console.log(car.animations);
+        scene.beginAnimation(car, 0, 200, true);
+
+        // Wheels animation
         const wheelRB = scene.getMeshByName("wheelRB");
         const wheelRF = scene.getMeshByName("wheelRF");
         const wheelLB = scene.getMeshByName("wheelLB");
