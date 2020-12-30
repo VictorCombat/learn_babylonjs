d2d53ed368ce4dcd212af38f546770a47bdf2db5
Victor COMBAT <victor.cmbt@gmail.com>
2020-12-30 19:21:01 +0100

    :sparkles: Chapter 5 - A Better Environment | Skys Above

--- Chapter 3 - Village Animation/script.js
+++ Chapter 3 - Village Animation/script.js
@@ -7,8 +7,19 @@ const createScene = function () {
 
     /* SET CAMERA & LIGHT */
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+    camera.upperBetaLimit = Math.PI / 2.2;
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+
+    // Skybox
+    const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 150 }, scene);
+    const skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("https://playground.babylonjs.com/textures/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
 
     const wireMat = new BABYLON.StandardMaterial("wireMat");
     wireMat.alpha = 0;
